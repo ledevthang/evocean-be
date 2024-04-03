1b3586c5dd9dea23641bfd55f7b0f1b949f2e7c6
the-ho-mang <anhmt@twendeesoft.com>
2024-04-03 13:35:58 +0700

    update theme schema

--- src/libs/prisma/prisma.module.ts
+++ src/libs/prisma/prisma.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 
 import { ThemeRepository } from "@root/shared/repositories/theme.repository";
 
@@ -6,6 +6,7 @@ import { PrismaService } from "./prisma.service";
 
 const providers = [PrismaService, ThemeRepository];
 
+@Global()
 @Module({
   providers: providers,
   exports: providers
