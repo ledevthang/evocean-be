86ec457e034b2bfb7c659ba3896f574936416ae4
the-ho-mang <anhmt@twendeesoft.com>
2024-05-02 18:30:37 +0700

    firebase setup

--- src/apis/theme/upload-theme.ts
+++ src/apis/theme/upload-theme.ts
@@ -1,7 +1,35 @@
-import Elysia from "elysia";
+import Elysia, { Static, t } from "elysia";
 
 import { ENDPOINT } from "@root/shared/constant";
 
+import {
+  FirebaseStorage,
+  getDownloadURL,
+  ref,
+  uploadBytes
+} from "firebase/storage";
+import { uploadFile } from "@root/services/firebase/upload";
+
+const payload = t.Object({
+  avatar: t.File({
+    type: ["image/png", "image/png"],
+    maxSize: 100_000_000
+  }),
+  background: t.File({
+    type: "image/png",
+    maxSize: 100_000_000
+  })
+});
+export type P = Static<typeof payload>;
+
 export const uploadTheme = new Elysia({
   name: "Handler.UploadTheme"
-}).post(ENDPOINT.THEME.UPLOAD_THEME, async ({}) => {});
+}).post(
+  ENDPOINT.THEME.UPLOAD_THEME,
+  async ({ body }) => {
+    await uploadFile(body.avatar);
+
+    return {};
+  },
+  { body: payload }
+);
---
+++ src/services/firebase/config.ts
@@ -0,0 +1,28 @@
+import { readConfigOrDie } from "@root/helpers/read-config";
+import { FileBlob } from "bun";
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { DateTime } from "luxon";
+
+const apiKey = readConfigOrDie("FIREBASE_API_KEY");
+
+const authDomain = readConfigOrDie("FIREBASE_AUTH_DOMAIN");
+
+const projectId = readConfigOrDie("FIREBASE_PROJECT_ID");
+
+const storageBucket = readConfigOrDie("FIREBASE_STORAGE_BUCKET");
+
+const messagingSenderId = readConfigOrDie("FIREBASE_MESSAGING_SENDER_ID");
+
+const appId = readConfigOrDie("FIREBASE_APP_ID");
+
+const app = initializeApp({
+  apiKey,
+  appId,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId
+});
+
+export const firebaseStorage = getStorage(app);
