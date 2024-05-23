6b017a0897e095f8b2c67f93386e4bf48d6b85e3
lainhathoang <nhathoang8c01@gmail.com>
2024-05-23 09:11:05 +0700

    update get price plugin & fix some issues

---
+++ src/services/http/get-crypto-price.ts
@@ -0,0 +1,20 @@
+import { readConfigOrDie } from "@root/helpers/read-config";
+import axios from "axios";
+
+type GetCryptoPrice {
+    token_id: string;
+    token_symbol: string;
+    price_usd: number;
+    lasted_updated: Date;
+}
+
+export const getCryptoPrice = async (token_id: string) : Promise<GetCryptoPrice> => {
+  const headers = {
+    x_cg_api_key: readConfigOrDie("COINGEKO_API_KEY")
+  };
+  const url = readConfigOrDie("COINGEKO_URL") + "/coins/" + token_id;
+
+  const response = await axios.get(url, { headers });
+
+  return response.data;
+};
