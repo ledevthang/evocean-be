ea2488c6ca514a00d5895b4eeb9fecfed304b6db
longnn198 <longnn@twendeesoft.com>
2024-08-13 16:32:43 +0700

    fix: upload feature icon

--- src/apis/theme/upload-feature-icon.ts
+++ src/apis/theme/upload-feature-icon.ts
@@ -3,8 +3,8 @@ import { StorageType, uploadFile } from "@root/services/firebase/upload";
 import { ENDPOINT } from "@root/shared/constant";
 
 const payload = t.Object({
-  file: t.File({
-    type: ["image/png", "image/png"],
+  icon: t.File({
+    type: ["image/jpg", "image/png"],
     maxSize: 100_000_000
   })
 });
@@ -14,7 +14,7 @@ export const uploadFeatureIcon = new Elysia({
 }).post(
   ENDPOINT.THEME.UPLOAD_FEATURE_ICON,
   async ({ body }) => {
-    const url = await uploadFile(body.file, StorageType.FEATURE_ICON);
+    const url = await uploadFile(body.icon, StorageType.FEATURE_ICON);
     return url;
   },
   { body: payload }
